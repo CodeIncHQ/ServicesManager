987eadc9e0a0cbfb4b48e88843e3da0c2e8b1ea9
Joan Fabrégat <joan@codeinc.fr>
2018-03-12 20:48:23 +0100

    bug fixes

--- src/Instantiator.php
+++ src/Instantiator.php
@@ -38,92 +38,92 @@ use CodeInc\Instantiator\Exceptions\ClassNotFoundException;
  */
 class Instantiator
 {
-	/**
-	 * Instantiated objects stack.
-	 *
-	 * @see Instantiator::addInstance()
-	 * @see Instantiator::getInstance()
-	 * @var object[]
-	 */
-	private $instances = [];
+    /**
+     * Instantiated objects stack.
+     *
+     * @see Instantiator::addInstance()
+     * @see Instantiator::getInstance()
+     * @var object[]
+     */
+    private $instances = [];
 
-	/**
-	 * Services aliases (other classes or interfaces)
-	 *
-	 * @see Instantiator::addAlias()
-	 * @var string[]
-	 */
-	private $aliases = [];
+    /**
+     * Services aliases (other classes or interfaces)
+     *
+     * @see Instantiator::addAlias()
+     * @var string[]
+     */
+    private $aliases = [];
 
-	/**
-	 * ServiceManager constructor.
-	 *
-	 * @throws InstantiatorException
-	 * @throws \ReflectionException
-	 */
-	public function __construct()
-	{
-		$this->addInstance($this);
-	}
+    /**
+     * ServiceManager constructor.
+     *
+     * @throws InstantiatorException
+     * @throws \ReflectionException
+     */
+    public function __construct()
+    {
+        $this->addInstance($this);
+    }
 
-	/**
-	 * Adds an instance of an object. This can be usefull when you need to make
+    /**
+     * Adds an instance of an object. This can be usefull when you need to make
      * available instances which requires a specific configuration like the
      * ServerRequest object or Doctrine's EntityManager.
-	 *
-	 * @param object $instance
-	 * @throws NotAnObjectException
-	 * @throws \ReflectionException
-	 */
-	public function addInstance($instance):void
-	{
-		// checks the serivce
-		if (!is_object($instance)) {
-			throw new NotAnObjectException(gettype($instance), $this);
-		}
+     *
+     * @param object $instance
+     * @throws NotAnObjectException
+     * @throws \ReflectionException
+     */
+    public function addInstance($instance):void
+    {
+        // checks the serivce
+        if (!is_object($instance)) {
+            throw new NotAnObjectException(gettype($instance), $this);
+        }
 
-		// adds the instance
-		$reflectionClass = new \ReflectionClass($instance);
-		$this->instances[$reflectionClass->getName()] = $instance;
+        // adds the instance
+        $reflectionClass = new \ReflectionClass($instance);
+        $this->instances[$reflectionClass->getName()] = $instance;
 
-		// maps the instance interfaces
-		foreach ($reflectionClass->getInterfaces() as $interface) {
-			$this->addAlias(
-			    $reflectionClass->getName(),
+        // maps the instance interfaces
+        foreach ($reflectionClass->getInterfaces() as $interface) {
+            $this->addAlias(
+                $reflectionClass->getName(),
                 $interface->getName(),
                 false
             );
-		}
+        }
 
-		// maps the instance parent classes
-		$parent = $reflectionClass;
-		while ($parent = $parent->getParentClass()) {
-			$this->addAlias(
-			    $reflectionClass->getName(),
+        // maps the instance parent classes
+        $parent = $reflectionClass;
+        while ($parent = $parent->getParentClass()) {
+            $this->addAlias(
+                $reflectionClass->getName(),
                 $parent->getName(),
                 false
             );
-		}
-	}
+        }
+    }
 
-	/**
-	 * Adds an alias for a class of an interface. This is the way to tell the
+    /**
+     * Adds an alias for a class of an interface. This is the way to tell the
      * service manager to use a specific class when it encounters an interface
      * as a type hint.
-	 *
-	 * @param string $class Class or interface name
-	 * @param string $alias
-	 * @param bool $replace
-	 * @return bool
-	 */
-	public function addAlias(string $class, string $alias,
+     *
+     * @param string $class Class or interface name
+     * @param string $alias
+     * @param bool $replace
+     * @return bool
+     */
+    public function addAlias(string $class, string $alias,
         bool $replace = true):bool
-	{
-		if ($replace === true || !isset($this->aliases[$alias])) {
-			$this->aliases[$alias] = $class;
-		}
-		return false;
-	}
+    {
+        if ($replace === true || !isset($this->aliases[$alias])) {
+            $this->aliases[$alias] = $class;
+        }
+        return false;
+    }
 
     /**
      * Verifies if an alias exist for a class / interface.
@@ -131,63 +131,62 @@ class Instantiator
      * @param string $class
      * @return bool
      */
-	public function hasAlias(string $class):bool
+    public function hasAlias(string $class):bool
     {
         return isset($this->aliases[$class]);
     }
 
-	/**
+    /**
      * Returns an alias target or null if not set.
      *
-	 * @param string $class
-	 * @return null|string
-	 */
-	public function getAlias(string $class):?string
-	{
-		return $this->aliases[$class] ?? null;
-	}
+     * @param string $class
+     * @return null|string
+     */
+    public function getAlias(string $class):?string
+    {
+        return $this->aliases[$class] ?? null;
+    }
 
-	/**
-	 * Returns an instance. If the instance was used previously, returns
+    /**
+     * Returns an instance. If the instance was used previously, returns
      * the previous one. If the instance was added using addInstance(),
      * returns the added instance.
-	 *
-	 * @param string $class
-	 * @return mixed
-	 * @throws ClassNotFoundException
-	 * @throws InterfaceWithoutAliasException
-	 * @throws NotAnObjectException
-	 * @throws InstantiatorException
-	 * @throws \ReflectionException
-	 */
-	public function getInstance(string $class)
-	{
-		// if there is an instance already available to the given class
-		if (isset($this->instances[$class])) {
-			return $this->instances[$class];
-		}
+     *
+     * @param string $class
+     * @return mixed
+     * @throws ClassNotFoundException
+     * @throws InterfaceWithoutAliasException
+     * @throws NotAnObjectException
+     * @throws InstantiatorException
+     * @throws \ReflectionException
+     */
+    public function getInstance(string $class)
+    {
+        // if the class is an alias
+        if ($alias = $this->getAlias($class)) {
+            $class = $alias;
+        }
 
-		// if the class is an alias
-		if ($alias = $this->getAlias($class)) {
-			$class = $alias;
-		}
+        // if there is an instance already available to the given class
+        if (isset($this->instances[$class])) {
+            return $this->instances[$class];
+        }
 
-		// checks if the class exists
-		if (!class_exists($class)) {
-			throw new ClassNotFoundException($class, $this);
-		}
+        // checks if the class is an interface
+        if (interface_exists($class)) {
+            throw new InterfaceWithoutAliasException($class, $this);
+        }
 
-		// checks if the class is an interface
-		$reflectionClass = new \ReflectionClass($class);
-		if ($reflectionClass->isInterface()) {
-			throw new InterfaceWithoutAliasException($class, $this);
-		}
+        // checks if the class exists
+        if (!class_exists($class)) {
+            throw new ClassNotFoundException($class, $this);
+        }
 
-		// if the class was never added or instantiated, we instantiate it
-		$instance = $this->instantiate($reflectionClass);
-		$this->addInstance($instance);
-		return $instance;
-	}
+        // if the class was never added or instantiated, we instantiate it
+        $instance = $this->instantiate($class);
+        $this->addInstance($instance);
+        return $instance;
+    }
 
     /**
      * Verifies the manager has an instance of an object.
@@ -195,7 +194,7 @@ class Instantiator
      * @param string $class
      * @return bool
      */
-	public function hasInstance(string $class):bool
+    public function hasInstance(string $class):bool
     {
         if (isset($this->instances[$class])) {
             return  true;
@@ -208,42 +207,42 @@ class Instantiator
         return false;
     }
 
-	/**
-	 * Alias of getInstance()
-	 *
-	 * @uses Instantiator::getInstance()
-	 * @param string $class
-	 * @return object
-	 * @throws InstantiatorException
-	 * @throws \ReflectionException
-	 */
-	public function __invoke(string $class)
-	{
-		return $this->getInstance($class);
-	}
+    /**
+     * Alias of getInstance()
+     *
+     * @uses Instantiator::getInstance()
+     * @param string $class
+     * @return object
+     * @throws InstantiatorException
+     * @throws \ReflectionException
+     */
+    public function __invoke(string $class)
+    {
+        return $this->getInstance($class);
+    }
 
-	/**
-	 * @param \ReflectionClass $class
-	 * @return object
-	 * @throws NewInstanceException
-	 * @throws InstantiatorException
-	 */
-	private function instantiate(\ReflectionClass $class)
-	{
-		try {
+    /**
+     * Returns a new instance of a class.
+     *
+     * @param string $class
+     * @return object
+     * @throws NewInstanceException
+     */
+    private function instantiate(string $class)
+    {
+        try {
+            $class = new \ReflectionClass($class);
             $args = [];
             foreach ($class->getMethod("__construct")->getParameters() as $number => $param) {
                 $args[] = $this->getCustructorParamValue($param, $class, $number + 1);
             }
-			return $class->newInstanceArgs($args);
-		}
-		catch (InstantiatorException $exception) {
-			throw $exception;
-		}
-		catch (\Throwable $exception) {
-            throw new NewInstanceException($class->getName(), $this);
-		}
-	}
+            return $class->newInstanceArgs($args);
+        }
+        catch (\Throwable $exception) {
+            throw new NewInstanceException($class->getName(),
+                $this, null, $exception);
+        }
+    }
 
     /**
      * Returns the value of a constructor parameter.
@@ -259,14 +258,14 @@ class Instantiator
      * @throws ParamValueException
      * @throws \ReflectionException
      */
-	private function getCustructorParamValue(\ReflectionParameter $param,
+    private function getCustructorParamValue(\ReflectionParameter $param,
         \ReflectionClass $class, int $number)
-	{
-		// if the param is required
-		if (!$param->isOptional()) {
+    {
+        // if the param is required
+        if (!$param->isOptional()) {
 
-			// if the param type is not set
-			if (!$param->hasType()) {
+            // if the param type is not set
+            if (!$param->hasType()) {
                 throw new ParamValueException(
                     $class->getName(),
                     $param->getName(),
@@ -274,10 +273,10 @@ class Instantiator
                     "the parameter does not have a type hint",
                     $this
                 );
-			}
+            }
 
-			// if the param type is not a class
-			if ($param->getType()->isBuiltin()) {
+            // if the param type is not a class
+            if ($param->getType()->isBuiltin()) {
                 throw new ParamValueException(
                     $class->getName(),
                     $param->getName(),
@@ -286,16 +285,16 @@ class Instantiator
                         $param->getType()->getName()),
                     $this
                 );
-			}
+            }
 
-			// instantiating the class
-			return $this->getInstance($param->getType()->getName());
-		}
+            // instantiating the class
+            return $this->getInstance($param->getType()->getName());
+        }
 
-		// optionnal param
-		else {
-			// if the param is optionnal returning it's default value
-			return $param->getDefaultValue();
-		}
-	}
+        // optionnal param
+        else {
+            // if the param is optionnal returning it's default value
+            return $param->getDefaultValue();
+        }
+    }
 }
